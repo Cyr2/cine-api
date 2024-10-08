0a93a873958516a783f6bfea9f0d402e19174933
Cyr2 <cyrigamingpro@gmail.com>
2024-10-08 13:55:40 +0200

    update index, and vercel.json for Access Control

--- public/index.php
+++ public/index.php
@@ -1,7 +1,8 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header('Access-Control-Allow-Credentials', true);
+header("Access-Control-Allow-Methods: GET");
+header('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     header("HTTP/1.1 200 OK");
