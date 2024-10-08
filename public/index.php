857827a90cd853b36ab8e7c6fd0f345e97ebbede
Cyr2 <cyrigamingpro@gmail.com>
2024-10-08 11:55:17 +0200

    update index, add condition and change access control

--- public/index.php
+++ public/index.php
@@ -1,8 +1,14 @@
 <?php
-require_once __DIR__ . '/../vendor/autoload.php';
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header("HTTP/1.1 200 OK");
+    exit();
+}
+
+require_once __DIR__ . '/../vendor/autoload.php';
 
 use Scraper\Scraper;
 
