fa37718354c5b637b4e501c8f7f383afdbcd20df
Cyr2 <cyrigamingpro@gmail.com>
2024-10-09 10:53:35 +0200

    update index Access Control

--- public/index.php
+++ public/index.php
@@ -1,8 +1,8 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-header('Access-Control-Allow-Credentials', 'true');
+header('Access-Control-Allow-Credentials: true');
 header("Access-Control-Allow-Methods: GET, OPTIONS, PATCH, DELETE, POST, PUT");
-header('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+header('Access-Control-Allow-Headers: X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     header("HTTP/1.1 200 OK");
@@ -30,4 +30,4 @@ try {
         'message' => $e->getMessage()
     ]);
 }
-
+?>
