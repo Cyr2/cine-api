0bb46a72be29932ed2b924e1a6ba64a75eb5862a
Cyr2 <cyrigamingpro@gmail.com>
2024-10-09 10:20:14 +0200

    update index for include try catch

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-header('Access-Control-Allow-Credentials', true);
-header("Access-Control-Allow-Methods: GET");
+header('Access-Control-Allow-Credentials', 'true');
+header("Access-Control-Allow-Methods: GET, OPTIONS, PATCH, DELETE, POST, PUT");
 header('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
@@ -13,13 +13,21 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use Scraper\Scraper;
 
-$url = 'https://pontlabbe.cineville.fr/programmes/pontlabbe';
-$scraper = new Scraper($url);
-$movies = $scraper->getMovies();
+try {
+    $url = 'https://pontlabbe.cineville.fr/programmes/pontlabbe';
+    $scraper = new Scraper($url);
+    $movies = $scraper->getMovies();
 
-header('Content-Type: application/json');
-echo json_encode([
-    'status' => 'success',
-    'data' => $movies
-]);
-?>
+    header('Content-Type: application/json');
+    echo json_encode([
+        'status' => 'success',
+        'data' => $movies
+    ]);
+} catch (Exception $e) {
+    header('Content-Type: application/json');
+    echo json_encode([
+        'status' => 'error',
+        'message' => $e->getMessage()
+    ]);
+}
+
